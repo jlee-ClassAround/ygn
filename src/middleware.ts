b540753a38808828f29908fff6938c9f7223a356
jlee-ClassAround <jlee@classaround.co.kr>
2026-01-08 18:16:54 +0900

    미들웨어 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,53 +1,61 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getSession } from "./lib/session";
+import { NextRequest, NextResponse } from 'next/server';
+import { getSession } from './lib/session';
 
 const matchRoute = (pathname: string, patterns: Set<string>) => {
-  return Array.from(patterns).some((pattern) => {
-    if (pattern.startsWith("^")) {
-      return new RegExp(pattern).test(pathname);
-    }
-    return pathname === pattern;
-  });
+    return Array.from(patterns).some((pattern) => {
+        if (pattern.startsWith('^')) {
+            return new RegExp(pattern).test(pathname);
+        }
+        return pathname === pattern;
+    });
 };
 
 // 로그인 유저 전용
 const authRoutes = new Set([
-  "^/courses/[^/]+/lessons/[^/]+",
-  "^/mypage(?:/.*)?$",
-  "^/checkout(?:/.*)?$",
+    '^/courses/[^/]+/lessons/[^/]+',
+    '^/mypage(?:/.*)?$',
+    '^/checkout(?:/.*)?$',
 ]);
 
 // 로그아웃 유저 전용
 const guestRoutes = new Set([
-  "^/login(?:/.*)?$",
-  "/kakao/start",
-  "/kakao/complete",
-  "/sign-up",
-  "/find-email",
-  "/find-pw",
+    '^/login(?:/.*)?$',
+    '/kakao/start',
+    '/kakao/complete',
+    '/sign-up',
+    '/find-email',
+    '/find-pw',
 ]);
 
 export default async function middleware(req: NextRequest) {
-  const pathname = req.nextUrl.pathname;
-  const search = req.nextUrl.search;
-  const session = await getSession();
-  const isLoggedIn = Boolean(session.id);
-  const isAuthRoute = matchRoute(pathname, authRoutes);
-  const isGuestRoute = matchRoute(pathname, guestRoutes);
+    const pathname = req.nextUrl.pathname;
 
-  if (!isLoggedIn && isAuthRoute) {
-    const loginUrl = new URL("/login", req.url);
-    loginUrl.searchParams.set("redirect", `${pathname}${search}`);
-    return NextResponse.redirect(loginUrl);
-  }
+    if (pathname === '/' || pathname.startsWith('/public-assets')) {
+        return NextResponse.next();
+    }
 
-  if (isLoggedIn && isGuestRoute) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+    const isAuthRoute = matchRoute(pathname, authRoutes);
+    const isGuestRoute = matchRoute(pathname, guestRoutes);
+
+    if (isAuthRoute || isGuestRoute) {
+        const session = await getSession();
+        const isLoggedIn = Boolean(session.id);
+
+        if (!isLoggedIn && isAuthRoute) {
+            const loginUrl = new URL('/login', req.url);
+            const search = req.nextUrl.search;
+            loginUrl.searchParams.set('redirect', `${pathname}${search}`);
+            return NextResponse.redirect(loginUrl);
+        }
+
+        if (isLoggedIn && isGuestRoute) {
+            return NextResponse.redirect(new URL('/', req.url));
+        }
+    }
+
+    return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.jpg$|.*\\.jpeg$|.*\\.svg$|.*\\.html$).*)",
-  ],
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\..*).*)'],
 };
